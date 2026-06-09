612be98c436c9933ad0b58f6a9b595066fbddc15
ABHISHEK DAS <abhishek24102016@gmail.com>
2026-06-09 17:14:37 +0530

    data added

--- src/extraction/extraction_service.py
+++ src/extraction/extraction_service.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 from azure.ai.documentintelligence import DocumentIntelligenceClient
 from azure.core.credentials import AzureKeyCredential
-from config import (AZURE_OCR_ENDPOINT, AZURE_OCR_API_KEY)
-
+from src.config.config import (
+    AZURE_OCR_ENDPOINT,
+    AZURE_OCR_API_KEY
+)
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 FILE_PATH = BASE_DIR / "data" / "filled accord form five.pdf"
@@ -30,10 +32,29 @@ def extract_text(pdf_path):
     return poller.result()
 
 
+
+def calculate_confidence(result):
+    confidences = []
+
+    for page in result.pages:
+        if page.words:
+            for word in page.words:
+                confidences.append(word.confidence)
+
+    if not confidences:
+        return 0
+
+    return sum(confidences) / len(confidences)
+
+
 def main():
     result = extract_text(FILE_PATH)
 
     print(json.dumps(result.as_dict(), indent=2))
+    confidence = calculate_confidence(result)
+
+    print(f"OCR Confidence: {confidence:.4f}")
+    print(f"OCR Confidence: {confidence*100:.2f}%")
 
 
 if __name__ == "__main__":
